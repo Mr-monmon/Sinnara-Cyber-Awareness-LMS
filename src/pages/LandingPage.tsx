aa5360df410c5d7dab4886c55d79b2c2c9914c8f
Muntasir Alhussain <136922687+Mr-monmon@users.noreply.github.com>
2026-04-03 02:34:42 +0300

    Increase logo height on LandingPage component 65

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -450,7 +450,7 @@ export const LandingPage = () => {
             <img
               src={LOGO}
               alt="AwareOne"
-              style={{ height: 48, width: "auto", display: "block" }}
+              style={{ height: 65, width: "auto", display: "block" }}
             />
           </a>
 
@@ -1419,7 +1419,7 @@ export const LandingPage = () => {
                 <img
                   src={LOGO}
                   alt="AwareOne"
-                  style={{ height: 48, width: "auto", display: "block" }}
+                  style={{ height: 65, width: "auto", display: "block" }}
                 />
               </div>
               <p
